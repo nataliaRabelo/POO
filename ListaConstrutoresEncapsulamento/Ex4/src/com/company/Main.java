4dcb928f7d7d552ed7008ee3816a80e90e7d05e7
Natália Bruno Rabelo <nataliabruno@id.uff.br>
2022-04-17 18:58:33 -0300

    Ajustes no exercicio 4

--- ListaConstrutoresEncapsulamento/Ex4/src/com/company/Main.java
+++ ListaConstrutoresEncapsulamento/Ex4/src/com/company/Main.java
@@ -4,11 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    Data data1 = new Data(31, 7, 1998);
-        Data data2  = new Data("Agosto", 8, 2017);
-        Data data3 = new Data(31, 1998);
+        Data data2 = new Data(15, 8, 1993);
+        Data data3  = new Data("Agosto", 8, 2017);
+        Data data4  = new Data("Julho", 7, 1998);
+        Data data5 = new Data(31, 1998);
+        Data data6 = new Data(15, 1993);
 
         System.out.println(data1.getDia() + " " + data1.getMes() + " " + data1.getAno());
-        System.out.println(data2.getMesPorExtenso() + " " + data2.getMes() + " " + data2.getAno());
-        System.out.println(data3.getDia() + " " +data3.getAno());
+        System.out.println(data2.getDia() + " " + data2.getMes() + " " + data2.getAno());
+        System.out.println(data3.getMesPorExtenso() + " " + data3.getMes() + " " + data3.getAno());
+        System.out.println(data4.getMesPorExtenso() + " " + data4.getMes() + " " + data4.getAno());
+        System.out.println(data5.getDia() + " " + data5.getAno());
+        System.out.println(data6.getDia() + " " + data5.getAno());
     }
 }
